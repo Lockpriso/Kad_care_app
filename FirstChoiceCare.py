c9b7e6ff5cdd2035c8d42cf02503fd2cd8f2cc30
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-03-26 19:24:23 +0200

    Update FirstChoiceCare.py

--- FirstChoiceCare.py
+++ FirstChoiceCare.py
@@ -7,11 +7,16 @@ import streamlit
 streamlit.title('🏤 👨‍⚕️ Kadige Care')
 streamlit.header('Votre santé. Notre passion.')
 streamlit.text('Des soins qui durent toute une vie !')
-streamlit.subheader("A propos de nous")
+streamlit.subheader("♌A propos de nous")
 streamlit.text("Bienvenue chez Kadige Care (KC)")
 streamlit.markdown("Nous sommes une structure de santé communautaire qui vise à fournir des services médicaux de haute qualité à la population locale." 
                   "Notre hôpital est dédié à répondre aux besoins de santé des personnes vivant dans les zones rurales, qui ont souvent peu" 
                   "d'accès aux services médicaux essentiels.")
+streamlit.markdown("Notre hôpital est plus qu'une simple installation médicale ; c'est une communauté. Nous sommes engagés à travailler en étroite" 
+                   "collaboration avec la communauté locale pour nous assurer que nos services sont adaptés à leurs besoins spécifiques. Nous croyons que"
+                   "la participation communautaire est cruciale pour améliorer les résultats en matière de santé, et nous accueillons les commentaires de "
+                   "nos patients et de leur famille. Merci de considérer notre hôpital pour vos besoins de santé. Nous sommes impatients de vous servir," 
+                   "vous et votre famille.")
 
 
 # Using "with" notation
