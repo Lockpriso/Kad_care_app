8a72f7e557c196e6f0e7688a5ccd07cfdc471522
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-03-26 17:48:09 +0200

    Update FirstChoiceCare.py

--- FirstChoiceCare.py
+++ FirstChoiceCare.py
@@ -1,4 +1,5 @@
 import streamlit
 
 streamlit.title('Kadige Care')
-streamlit.header('Your health, out passion. Care That Lasts a Lifetime')
+streamlit.header('Your health. Our passion.')
+streamlit.subheader('Care That Lasts a Lifetime!')
