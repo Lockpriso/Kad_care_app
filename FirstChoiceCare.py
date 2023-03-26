911492267afd05bbeb231c406fed4a7fd2aa404d
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-03-26 19:18:31 +0200

    Update FirstChoiceCare.py

--- FirstChoiceCare.py
+++ FirstChoiceCare.py
@@ -5,10 +5,11 @@ import streamlit
 
 
 streamlit.title('🏤 👨‍⚕️ Kadige Care')
-streamlit.header('Votre santé. Notre passion. Des soins qui durent toute une vie !')
+streamlit.header('Votre santé. Notre passion.)
+streamlit.text('Des soins qui durent toute une vie !')
 streamlit.subheader("A propos de nous")
 streamlit.text("Bienvenue chez Kadige Care (KC)")
-streamlit.text("Nous sommes une structure de santé communautaire qui vise à fournir des services médicaux de haute qualité à la population locale." 
+streamlit.markdown("Nous sommes une structure de santé communautaire qui vise à fournir des services médicaux de haute qualité à la population locale." 
                   "Notre hôpital est dédié à répondre aux besoins de santé des personnes vivant dans les zones rurales, qui ont souvent peu" 
                   "d'accès aux services médicaux essentiels.")
 
