b215fe96491c59cb9ba455cd56f13f9d5e31fe1e
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-03-26 18:28:30 +0200

    Update FirstChoiceCare.py

--- FirstChoiceCare.py
+++ FirstChoiceCare.py
@@ -8,10 +8,17 @@ streamlit.title('Kadige Care')
 streamlit.header('Your health. Our passion.')
 streamlit.subheader('Care That Lasts a Lifetime!')
 
-streamlit.sidebar.title("Órbitas em Relatividade")
-pagina_selecionada = streamlit.sidebar.selectbox("Selecione um tipo de órbita", ['Óbita de corpos celestes', 'Órbita de raios de luz'])
-if pagina_selecionada == "Óbita de corpos celestes":
-    streamlit.title("Particula massiva orbitando um buraco negro com a massa do Sol")
-    streamlit.write("E se, de repente, o Sol se transformasse em um buraco negro?")
-    streamlit.write("Para isso, toda sua massa, de 2 $\cdot$ 10$^{30}$ kg (hoje espalhada numa esfera com cerca de  700.000 km de raio), deveria ser comprimida numa região com raio de cerca de  3  km.")
-   
+# Using object notation
+add_selectbox = streamlit.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with streamlit.sidebar:
+    add_radio = streamlit.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+
